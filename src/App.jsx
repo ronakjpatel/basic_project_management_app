db9079ce105d23d7ed721929ecfbbd3da514c41f
ronakjpatel <rjpatel7991@gmail.com>
2024-06-06 12:32:25 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="my-8 text-center text-xl font-bold text-slate-600">
+        Hello World
+      </h1>
+    </>
+  );
+}
+
+export default App;
